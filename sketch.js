0348aba2f393e4cdda42091393ebfab8828ae51b
thesunthesun <90666571+thesunthesun@users.noreply.github.com>
2022-06-02 14:27:51 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,43 @@
+var x;
+var y;
+var centerx;
+var centery;
+var theta = 0;
+var ropelength = 100;
+var green = 0;
+var diameter = 50;
+
 function setup() {
   // put setup code here
+  createCanvas(400,500);
+  background(255);  
 }
 
 function draw() {
   // put drawing code here
+  centerx = width/2;
+  centery = height/2;
+  background(255);
+  fill(0,green,0);
+  circle(x,y,diameter);
+  line(centerx, centery, x, y);
+  theta += 1;
+  if (theta % 60 == 0) {
+    green += 10;
+  }
+  if (green > 255) {
+    green = 0;
+  }
+  x = newX(ropelength, centerx, theta) ;
+  y = newY(ropelength, centery, theta) ;
+}
+
+var newX(var ropelength, var centerx, var theta) {
+  float new_x = centerx + ropelength * (cos(radians(theta)));
+  return  new_x;
+}
+
+var newY(var ropelength, var centery, var theta) {
+  float new_y = centery + ropelength * (sin(radians(theta)));
+  return  new_y;
 }
